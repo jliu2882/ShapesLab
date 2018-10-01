2a596e618bb74ebc759deaf19f4ebad525109c4e
Jack <jliu2882@bths.edu>
2018-10-01 11:09:56 -0400

    Shape created

--- src/groupWork/Main.java
+++ src/groupWork/Main.java
@@ -5,4 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here kevin is trash at being trash 
     }
+    public abstract class Shape{
+        private String type;
+
+        public Shape(String type) {
+            this.type = type;
+        }
+
+        public abstract double getArea();
+
+        public abstract double getPerimeter();
+
+        public String toString() {
+            return "I am a " + type + ".";
+        }
+    }
 }
